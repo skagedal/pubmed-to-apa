389a08d6e4c4bf9cb7e705f8771a0415b44bb94e
Simon Kågedal Reimer <skagedal@gmail.com>
2012-09-18 12:40:58 +0200

    Add license (MIT). Link to github page.

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
+<!--
+Copyright (c) 2008-2012 Simon Kågedal Reimer
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+-->
 <html>
 <head>
 <title>PubMed ID to APA reference</title>
@@ -89,7 +111,8 @@ input_form();
 
 ?>
 <hr />
-By <a href="http://helgo.net/simon/">Simon K&aring;gedal</a>, 2008
+By <a href="http://helgo.net/simon/">Simon K&aring;gedal</a>, 2008 &ndash; 
+<a href="https://github.com/skagedal/pubmed-to-apa">source code at github</a> (licensed with <a href="http://opensource.org/licenses/MIT">MIT License</a>)
 </div> <!-- maincontent -->
 </body>
 </html>
